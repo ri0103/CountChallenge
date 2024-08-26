2bcbc3b07f11cd1f6ac89f0710861200d6c94010
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-26 11:43:29 +0900

    お題１

---
+++ app/src/main/java/app/ishizaki/dragon/countchallenge/MainActivity.kt
@@ -0,0 +1,23 @@
+package app.ishizaki.dragon.countchallenge
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import app.ishizaki.dragon.countchallenge.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        var countNumber: Int = 0
+
+        binding.countButton.setOnClickListener {
+            countNumber += 1
+            binding.countText.text = countNumber.toString()
+        }
+    }
+}
