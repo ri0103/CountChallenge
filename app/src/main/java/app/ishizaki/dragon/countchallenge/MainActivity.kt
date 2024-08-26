90029dee98520f58ca1318dbccd3e89ee53c0f8c
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-26 12:42:21 +0900

    お題４

--- app/src/main/java/app/ishizaki/dragon/countchallenge/MainActivity.kt
+++ app/src/main/java/app/ishizaki/dragon/countchallenge/MainActivity.kt
@@ -1,7 +1,8 @@
 package app.ishizaki.dragon.countchallenge
 
-import android.content.res.ColorStateList
 import android.graphics.Color
+import android.media.AudioManager
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import app.ishizaki.dragon.countchallenge.databinding.ActivityMainBinding
@@ -14,13 +15,21 @@ class MainActivity : AppCompatActivity() {
 
         var countNumber: Int = 0
 
+        val soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+
+        val soundId: Int = soundPool.load(this, R.raw.sound, 1)
+
+
         binding.countButton.setOnClickListener {
             countNumber += 1
             binding.countText.text = countNumber.toString()
 
             when (countNumber%2==0) {
                 true -> binding.countText.setTextColor(Color.BLUE)
-                false -> binding.countText.setTextColor(Color.RED)
+                false -> {
+                    binding.countText.setTextColor(Color.RED)
+                    soundPool.play(soundId, 1.0F, 1.0F, 0, 0, 1.0F)
+                }
             }
 
         }
