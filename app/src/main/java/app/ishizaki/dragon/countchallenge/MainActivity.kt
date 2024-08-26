75811a1238bc2158140d26cec3a622892cd3b000
ri0103 <ryu2006ishizaki@gmail.com>
2024-08-26 12:50:15 +0900

    お題５

--- app/src/main/java/app/ishizaki/dragon/countchallenge/MainActivity.kt
+++ app/src/main/java/app/ishizaki/dragon/countchallenge/MainActivity.kt
@@ -1,5 +1,6 @@
 package app.ishizaki.dragon.countchallenge
 
+import android.content.Context
 import android.graphics.Color
 import android.media.AudioManager
 import android.media.SoundPool
@@ -9,29 +10,36 @@ import app.ishizaki.dragon.countchallenge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
-        var countNumber: Int = 0
+        val sharedPref = getSharedPreferences("COUNT_PREFERENCE", Context.MODE_PRIVATE)
+
+        var countNumber = sharedPref.getInt("COUNT", 0)
 
         val soundPool = SoundPool(1, AudioManager.STREAM_MUSIC, 0)
-
         val soundId: Int = soundPool.load(this, R.raw.sound, 1)
 
+        applyCountText(countNumber, soundPool, soundId)
 
         binding.countButton.setOnClickListener {
             countNumber += 1
-            binding.countText.text = countNumber.toString()
+            applyCountText(countNumber, soundPool, soundId)
+            sharedPref.edit().putInt("COUNT", countNumber).apply()
+        }
+    }
 
-            when (countNumber%2==0) {
-                true -> binding.countText.setTextColor(Color.BLUE)
-                false -> {
-                    binding.countText.setTextColor(Color.RED)
-                    soundPool.play(soundId, 1.0F, 1.0F, 0, 0, 1.0F)
-                }
+    fun applyCountText(countNumber: Int, soundPool: SoundPool, soundId: Int){
+        binding.countText.text = countNumber.toString()
+
+        when (countNumber%2==0) {
+            true -> binding.countText.setTextColor(Color.BLUE)
+            false -> {
+                binding.countText.setTextColor(Color.RED)
+                soundPool.play(soundId, 1.0F, 1.0F, 0, 0, 1.0F)
             }
-
         }
     }
 }
